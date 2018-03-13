e1d0bafc292c54f3584f507034cd7147169ee73e
Ben Guiden <benguiden@hotmail.co.uk>
2018-03-13 09:24:23 +0000

    Added boid componenet.

---
+++ Assets/Script/Boid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    #region Public Variables
+    [Header("Physics")]
+    public float mass = 1f;
+    public float maxSpeed = 10f;
+
+    [HideInInspector]
+    public Vector3 velocity, acceleration;
+    #endregion
+
+    #region Mono Methods
+    void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + velocity);
+    }
+    #endregion
+
+}
